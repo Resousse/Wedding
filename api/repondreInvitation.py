d50c974b7f39e12214c563ee4cef5009b144e27a
Rapha <raphaelbensussan@macbook-pro-de-rapha.home>
2020-11-24 22:23:08 +0100

    envoi API

---
+++ api/repondreInvitation.py
@@ -0,0 +1,43 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from datetime import datetime
+from threading import Thread
+import requests
+import json
+import urllib.parse
+
+class handler(BaseHTTPRequestHandler):
+  def do_POST(self):
+    self.send_header('Content-type', 'text/plain')
+    self.end_headers()
+    try:
+        length = int(self.headers.get('content-length', 0))
+        body = self.rfile.read(length)
+        url = 'https://api.trello.com/1/cards?key=c2d06be31329a6038f5e296a9759bd50&token=31609cd6094852d16850a30962bf8516f9f40b01fda773fa6f3a99d1eb72dc89&idList=5fbd68c43cd0dc77f36dcd50&name={}&desc={}'
+
+        js = json.loads(body)
+        nom = "Famille {} {}".format(js['Prenom'], js['Nom'])
+        
+        description = "{}\n{}\nDate réponse : {}".format(js['Prenom']+ js['Nom'], js['Invités'], js['Date'])
+        url = url.format(urllib.parse.quote_plus(nom), urllib.parse.quote_plus(description))
+        r = requests.post(url)
+        if r:
+            rep = json.loads(r.text)
+            urllab = 'https://api.trello.com/1/cards/{}/idLabels?key=c2d06be31329a6038f5e296a9759bd50&token=31609cd6094852d16850a30962bf8516f9f40b01fda773fa6f3a99d1eb72dc89&value={}'
+            stat = '5fbd66ce1258da48af20be7f'
+            if js['Statut'] == 'Viennent':
+                stat = '5fbd66ce1258da48af20be79'
+            urllab = urllab.format(rep['id'], stat)
+            s = requests.post(urllab)
+            if s:
+                self.wfile.write(str("OK").encode("utf-8"))
+                self.send_response(200)
+                return
+    except:
+        pass
+    self.wfile.write(str("KO").encode("utf-8"))
+    self.send_response(404)
+
+if __name__ == "__main__":
+    server = HTTPServer(('', 8001), handler)
+    server.url = 'http://localhost:{}'.format(server.server_port)
+    server.serve_forever() 
